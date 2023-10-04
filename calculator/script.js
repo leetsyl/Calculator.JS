178067bfec0c66e37718f0a38296955cfe810abc
leetsyl <leecomm2@gmail.com>
2023-10-04 19:31:41 -0300

    new changes

--- calculator/script.js
+++ calculator/script.js
@@ -2,7 +2,25 @@ const operators = ['*', '+', '-', '/'];
 
 var firstNumber = true;
 
+var btn = document.getElementsByClassName("btn-num");
+var box = document.getElementById("ipt-num");
+
 function buttonNumber(button) {
-    alert(button);
-    
+    if (!(operators.includes(button))) {
+        if (firstNumber) {
+            box.value = button;
+            console.log(button);
+            
+            firstNumber = false;
+        
+        } else {
+            box.value = box.value + button;
+        }
+        
+    }
 };
+
+function allClear() {
+    box.value = "";
+}
+
